f1616e5b10654e6fa23c96f168159bb91d872a11
valde1992 <fervaldex1992@gmail.com>
2016-11-03 00:25:02 -0300

    Agrego el modelo del workbench y el sql. Se agrega el modelo User y con el registro y logueo

---
+++ sql/salesiano.sql
@@ -0,0 +1,378 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 03-11-2016 a las 00:22:07
+-- Versión del servidor: 5.7.16-0ubuntu0.16.04.1
+-- Versión de PHP: 7.0.8-0ubuntu0.16.04.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `salesiano`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clasificaciones`
+--
+
+CREATE TABLE `clasificaciones` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `fondo` int(10) UNSIGNED NOT NULL,
+  `usuario_carga` int(10) UNSIGNED NOT NULL,
+  `fecha_carga` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `donaciones`
+--
+
+CREATE TABLE `donaciones` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `donante` int(10) UNSIGNED NOT NULL,
+  `pieza` int(10) UNSIGNED NOT NULL,
+  `fecha_donacion` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `donantes`
+--
+
+CREATE TABLE `donantes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `persona` int(10) UNSIGNED NOT NULL,
+  `fecha_carga_donante` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fondos`
+--
+
+CREATE TABLE `fondos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `usuario_carga` int(10) UNSIGNED NOT NULL,
+  `fondo_carga` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fotos`
+--
+
+CREATE TABLE `fotos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `pieza` int(10) UNSIGNED NOT NULL,
+  `foto` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE `personas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `cuil_cuit` varchar(13) DEFAULT NULL,
+  `domicilio` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `fecha_carga` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `piezas`
+--
+
+CREATE TABLE `piezas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `clasificacion` int(10) UNSIGNED NOT NULL,
+  `procedencia` varchar(100) DEFAULT NULL,
+  `autor` varchar(100) DEFAULT NULL,
+  `fecha_ejecucion` date DEFAULT NULL,
+  `tema` varchar(100) DEFAULT NULL,
+  `observacion` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `revisiones`
+--
+
+CREATE TABLE `revisiones` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `usuario_revision` int(10) UNSIGNED DEFAULT NULL,
+  `pieza` int(10) UNSIGNED NOT NULL,
+  `fecha_revision` date DEFAULT NULL,
+  `estado_conservacion` varchar(100) DEFAULT NULL,
+  `ubicacion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `persona` int(10) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clasificaciones`
+--
+ALTER TABLE `clasificaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_clasificaciones_users1_idx` (`usuario_carga`),
+  ADD KEY `fk_clasificaciones_fondo1_idx` (`fondo`);
+
+--
+-- Indices de la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_donaciones_donantes1_idx` (`donante`),
+  ADD KEY `fk_donaciones_piezas1_idx` (`pieza`);
+
+--
+-- Indices de la tabla `donantes`
+--
+ALTER TABLE `donantes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_donantes_personas1_idx` (`persona`);
+
+--
+-- Indices de la tabla `fondos`
+--
+ALTER TABLE `fondos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_fondo_users1_idx` (`usuario_carga`);
+
+--
+-- Indices de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_fotos_piezas1_idx` (`pieza`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`),
+  ADD KEY `password_resets_token_index` (`token`);
+
+--
+-- Indices de la tabla `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `piezas`
+--
+ALTER TABLE `piezas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_piezas_clasificaciones1_idx` (`clasificacion`);
+
+--
+-- Indices de la tabla `revisiones`
+--
+ALTER TABLE `revisiones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_revisiones_users1_idx` (`usuario_revision`),
+  ADD KEY `fk_revisiones_piezas1_idx` (`pieza`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD KEY `fk_users_personas_idx` (`persona`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clasificaciones`
+--
+ALTER TABLE `clasificaciones`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `donantes`
+--
+ALTER TABLE `donantes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `fondos`
+--
+ALTER TABLE `fondos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `personas`
+--
+ALTER TABLE `personas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `piezas`
+--
+ALTER TABLE `piezas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `revisiones`
+--
+ALTER TABLE `revisiones`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `clasificaciones`
+--
+ALTER TABLE `clasificaciones`
+  ADD CONSTRAINT `fk_clasificaciones_fondo1` FOREIGN KEY (`fondo`) REFERENCES `fondos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_clasificaciones_users1` FOREIGN KEY (`usuario_carga`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  ADD CONSTRAINT `fk_donaciones_donantes1` FOREIGN KEY (`donante`) REFERENCES `donantes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_donaciones_piezas1` FOREIGN KEY (`pieza`) REFERENCES `piezas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `donantes`
+--
+ALTER TABLE `donantes`
+  ADD CONSTRAINT `fk_donantes_personas1` FOREIGN KEY (`persona`) REFERENCES `personas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `fondos`
+--
+ALTER TABLE `fondos`
+  ADD CONSTRAINT `fk_fondo_users1` FOREIGN KEY (`usuario_carga`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD CONSTRAINT `fk_fotos_piezas1` FOREIGN KEY (`pieza`) REFERENCES `piezas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `piezas`
+--
+ALTER TABLE `piezas`
+  ADD CONSTRAINT `fk_piezas_clasificaciones1` FOREIGN KEY (`clasificacion`) REFERENCES `clasificaciones` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `revisiones`
+--
+ALTER TABLE `revisiones`
+  ADD CONSTRAINT `fk_revisiones_piezas1` FOREIGN KEY (`pieza`) REFERENCES `piezas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_revisiones_users1` FOREIGN KEY (`usuario_revision`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_users_personas` FOREIGN KEY (`persona`) REFERENCES `personas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
