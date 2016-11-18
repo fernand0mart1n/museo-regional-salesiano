4afe5b615686da91c8d04cf2ae6e8baadc1d13e4
valde1992 <fernando_valde@hotmail.com>
2016-11-18 09:45:44 -0300

    Detalles al registro de usuario

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,6 +3,7 @@
 @section('head')
 
 <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
+<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" media="all" />
 
 @endsection
 
@@ -17,7 +18,7 @@
                 <h2> Registro </h2> 
                 <p> 
                     <!--div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}"-->
-                    <label for="usernamesignup" class="uname" data-icon="u"><span>Tu nombre de usuario</span></label>
+                    <label for="usernamesignup" class="uname" data-icon=""><span>Tu nombre de usuario</span></label>
                     <input id="usernamesignup" name="name" required="required" value="{{ old('name') }}" type="text" placeholder="Usuario" />
                     @if ($errors->has('name'))
                         <span class="help-block">
@@ -27,7 +28,7 @@
                 </p>
                 <p> 
                     <!--div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}"-->
-                    <label for="emailsignup" class="youmail" data-icon="e" ><span>Tu email</span></label>
+                    <label for="emailsignup" class="youmail" data-icon="" ><span>Tu email</span></label>
                     <input id="emailsignup" name="email" required="required" value="{{ old('email') }}" type="email" placeholder="ejemplo@mail.com"/> 
                     @if ($errors->has('email'))
                         <span class="help-block">
@@ -36,46 +37,56 @@
                     @endif
                 </p>
                 <p> 
+                    <label for="nombre" class="youpasswd" data-icon=""><span>Confirma la contraseña</span></label>
+                    <input id="passwordsignup_confirm" name="password_confirmation" required="required" type="password" placeholder="Confirma la contraseña"/>
+                </p>
+
+                <p> 
+                    <label for="passwordsignup_confirm" class="youpasswd" data-icon=""><span>Confirma la contraseña</span></label>
+                    <input id="passwordsignup_confirm" name="password_confirmation" required="required" type="password" placeholder="Confirma la contraseña"/>
+                </p>
+
+                <p> 
                     <!--div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}"-->
-                    <label for="passwordsignup" class="youpasswd" data-icon="p"><span>Tu contraseña</span></label>
-                    <input id="passwordsignup" name="password" required="required" type="password" placeholder="Contraseña"/>
-                    @if ($errors->has('password'))
+                    <label for="passwordsignup" data-icon=""><span>Nombre(s)</span></label>
+                    <input id="nombre" name="nombre" required="required" type="text" placeholder="Nombre(s)"/>
+                    @if ($errors->has('nombre'))
                         <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
+                            <strong>{{ $errors->first('nombre') }}</strong>
                         </span>
                     @endif
                 </p>
                 <p> 
-                    <label for="passwordsignup_confirm" class="youpasswd" data-icon="p"><span>Confirma la contraseña</span></label>
-                    <input id="passwordsignup_confirm" name="password_confirmation" required="required" type="password" placeholder="Confirma la contraseña"/>
+                    <label for="apellido" data-icon=""><span>Apellido(s)</span></label>
+                    <input id="apellido" name="apellido" required="required" type="text" placeholder="Apellido(s)"/>
                 </p>
-
-                <?= Former::text('nombre')
-                ->placeholder('Nombre') ?>
-
-                <?= Former::text('apellido')
-                ->placeholder('Apellido') ?>
 
                 <label for="sexo">Sexo</label><br>
                 <input type="checkbox" name="sexo" value="Masculino">
-                <label for="sexo"><span></span> Masculino</label><br>
+                <label for="sexo"><span>Masculino <i class="fa fa-male"></i></span></label><br>
                  
                 <input type="checkbox" name="sexo" value="Car">
-                <label for="sexo"><span></span> Femenino</label><br><br>
+                <label for="sexo"><span>Femenino <i class="fa fa-female"></i></span></label><br><br>
 
-                <?= Former::text('cuil_cuit')
-                ->label('CUIL/CUIT')
-                ->placeholder('CUIL/CUIT') ?>
+                <p> 
+                    <label for="cuil_cuit" data-icon=""><span>CUIL/CUIT</span></label>
+                    <input id="cuil_cuit" name="cuil_cuit" required="required" type="text" placeholder="CUIL/CUIT"/>
+                </p>
 
-                <?= Former::text('domicilio')
-                ->placeholder('Domicilio') ?>
+                <p> 
+                    <label for="domicilio" data-icon=""><span>Domicilio</span></label>
+                    <input id="domicilio" name="domicilio" required="required" type="text" placeholder="Domicilio"/>
+                </p>
 
-                <?= Former::text('telefono')
-                ->label('Teléfono')
-                ->placeholder('Teléfono') ?>
+                <p> 
+                    <label for="telefono" data-icon=""><span>Teléfono</span></label>
+                    <input id="telefono" name="telefono" required="required" type="text" placeholder="Teléfono"/>
+                </p>
 
-                <?= Former::date('fecha_carga')
-                ->label('Fecha de carga') ?>
+                <p> 
+                    <label for="fecha_carga" data-icon=""><span>Fecha de carga</span></label>
+                    <input id="fecha_carga" name="fecha_carga" required="required" type="text" placeholder="Fecha de carga"/>
+                </p>
 
                 <p class="signin button"> 
                     <input type="submit" value="Registrar"/> 
@@ -86,3 +97,4 @@
                 </p>
             </form>
         </div>
+@endsection
