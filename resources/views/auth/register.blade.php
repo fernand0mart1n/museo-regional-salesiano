558c21ff86fc3da4996787784057d279572a08c0
valde1992 <fervaldex1992@gmail.com>
2016-11-18 15:26:18 -0300

    Agrego FA al registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,6 +3,7 @@
 @section('head')
 
 <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
+<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" media="all" />
 
 @endsection
 
